75032d859ae776abf4276eb0baa9799bdacbb9a6
Emelda Torren <etorren@ev-h2.kzn.21-school.ru>
2020-08-10 17:28:33 +0300

    work logic v2

--- logic.c
+++ logic.c
@@ -6,11 +6,12 @@
 /*   By: masharla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/10 10:32:04 by masharla          #+#    #+#             */
-/*   Updated: 2020/08/10 14:07:33 by masharla         ###   ########.fr       */
+/*   Updated: 2020/08/10 18:00:03 by etorren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
+#include <stdio.h>
 
 char		min(char m1, char m2, char m3)
 {
@@ -19,17 +20,8 @@ char		min(char m1, char m2, char m3)
 	tmp = m1;
 	(tmp > m2 ? tmp = m2 : 1);
 	(tmp > m3 ? tmp = m3 : 1);
+	printf("==> min OK\n");
 	return (tmp);
-}
-
-long int	count_cols(char **map)
-{
-	int i;
-	
-	i = 0;
-	while (map[0][i])
-		i++;
-	return (i);
 }
 
 long int	count_rows(char **map)
@@ -37,8 +29,22 @@ long int	count_rows(char **map)
 	int i;
 	
 	i = 0;
+	printf("==> count_rows HERE");
 	while (map[i][0])
 		i++;
+	printf("==> count_rows OK: rows = %d\n", i);
+	return (i);
+}
+
+long int	count_cols(char **map)
+{
+	int i;
+	
+	i = 0;
+	printf("==> count_cols HERE");
+	while (map[0][i])
+		i++;
+	printf("==> count_cols OK: cols = %d\n", i);
 	return (i);
 }
 
@@ -50,25 +56,45 @@ char **build_submap(long int rows, long int cols, char **map)
 
 	i = 0;
 	j = 0;
-	submap = malloc(rows * cols);
-	while (i < rows)
+	submap = (char **)malloc(sizeof(submap) * rows);
+	while (i < rows - 1)
 	{
+		submap[i] = (char *)malloc(sizeof(char) * cols);
 		submap[i][0] = map[1 + i][0];
 		i++;
 	}
-	while (i < cols)
+	submap[i] = "\0";
+	printf("==> submap rows OK: записано элементов: %d\n", i);
+	while (j < cols)
 	{
 		submap[0][j] = map[1][j];
 		j++;
 	}
+	printf("==> submap cols OK: записано элементов: %d\n", j);
 	i = 1;
 	j = 1;
-	while (i < rows)
+	while (i < rows - 1)
+	{
+		while(j < cols)
 		{
-			while(j < cols)
-				(map[i + 1][j] == map[0][1] ? (submap[i][j] = min(submap[i][j-1],\
-				submap[i-1][j], submap[i-1][j-1])) : (submap[i][j] = map[0][2]));
+			printf("j++\n");
+			(map[i + 1][j] == map[0][1] ? (submap[i][j] = min(submap[i][j-1],\
+			submap[i-1][j], submap[i-1][j-1]) + 1) : ((submap[i][j] = map[0][2]) && printf("==> min SKIP\n")));
+			j++;
 		}
+	//	submap[i][j] = '\0';
+		i++;
+		j = 1;
+		printf("i++\n");
+	}
+	// delete >>
+	i = 0;
+	while (i < rows - 1)
+	{
+		printf("%s\n", submap[i]);
+		i++;
+	}
+	//delete <<
 	return (submap);
 }
 
--- main.c
+++ main.c
@@ -6,46 +6,69 @@
 /*   By: masharla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/10 10:13:30 by masharla          #+#    #+#             */
-/*   Updated: 2020/08/10 13:26:01 by masharla         ###   ########.fr       */
+/*   Updated: 2020/08/10 18:00:06 by etorren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
+#include <stdio.h>
 
 int		main(int argc, char **argv)
 {
 	(void) argc;
 	(void) argv;
 	char **map;
+	int i;
 
-	map = malloc(sizeof(map));
+	i = 0;
+	map = NULL;
+	map = (char **)malloc(sizeof(map) * 6);
+	while (i <= 4)
+	{
+		map[i] = (char *)malloc(sizeof(char) * 5);
+		printf("==> malloc map[%d] used\n", i);
+		i++;
+	}
 //	if (argc < 2)
 //		map = std_input();
 //	else
 //		map = file_input();
 //	while (*map)
 //		find_biggest(map);
-	
-	map[0][0] = 4;
+
+/*	map[0][0] = '4';
 	map[0][1] = '.';
 	map[0][2] = 'o';
 	map[0][3] = 'x';
+	map[0][4] = '\0';
 	map[1][0] = '.';
 	map[1][1] = 'o';
 	map[1][2] = 'o';
 	map[1][3] = 'o';
+	map[1][4] = '\0';
 	map[2][0] = 'o';
 	map[2][1] = '.';
 	map[2][2] = '.';
 	map[2][3] = '.';
+	map[2][4] = '\0';
 	map[3][0] = '.';
 	map[3][1] = '.';
 	map[3][2] = '.';
 	map[3][3] = '.';
+	map[3][4] = '\0';
 	map[4][0] = 'o';
 	map[4][1] = 'o';
 	map[4][2] = '.';
 	map[4][3] = 'o';
+	map[4][4] = '\0';
+	map[5][0] = '\0';*/
 
+	map[0] = "4.ox\0";
+	map[1] = ".+++\0";
+	map[2] = "+...\0";
+	map[3] = "....\0";
+	map[4] = "++.+\0";
+	map[5] = "\0";
+	printf("==> map created\n");
 	find_biggest(map);
 }
