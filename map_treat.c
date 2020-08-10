3af2af772f62097b27c6fcab57732e8343a3ea3b
Machoke Sharla <masharla@ev-h4.kzn.21-school.ru>
2020-08-10 18:08:25 +0300

    map treatment

--- map_treat.c
+++ map_treat.c
@@ -6,7 +6,7 @@
 /*   By: masharla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/10 13:53:19 by masharla          #+#    #+#             */
-/*   Updated: 2020/08/10 14:07:29 by masharla         ###   ########.fr       */
+/*   Updated: 2020/08/10 18:03:14 by masharla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,24 +33,47 @@ int		find_max(long int rows, long int cols, char **submap)
 	return (max);
 }
 
-int		search_i(char max, char **submap)
+char	**fill_map(char max, char **submap, char **map)
 {
 	int i;
 	int j;
+	int	i_size;
+	int j_size;
 
 	i = 0;
 	j = 0;
-	while (char 
+	i_size = max - map[0][1];
+	j_size = max - map[0][1];
+	while (submap[i++])
+		while (submap[i][j++])
+			if (submap[i][j] == max)
+				while (i_size--)
+				{
+					while(j_size--)
+					{
+						map[i + 1][j] = max;
+						j--;
+					}
+					j += (max - map[0][1]);
+					i--;
+				}
 }
 
-void	print_result(char max, char **submap)
+char	**convert_obs(char **submap, char **map)
 {
 	int i;
 	int j;
-	int i_needed;
-	int j_needed;
 
 	i = 0;
 	j = 0;
-	i_needed = 
+	while (submap[i])
+	{
+		while (submap[i][j])
+		{
+			if (submap[i][j] == map[0][2])
+				submap[i][j] = 31;
+			j++;
+		}
+		i++;
+	}
 }
